3d3ae48836c182006e086b078149e6a5a20009f7
Praney Behl <praneybehl@gmail.com>
2015-11-24 10:09:10 +1100

    updated meteor package name

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
-  name: 'praneybehl:react-hammerjs',
-  version: '0.4.3',
+  name: 'praneybehl:reactjs-hammerjs',
+  version: '0.4.2',
   summary: 'ReactJS - HammerJS integration. Support touch events in your Meteor app.',
   git: 'https://github.com/praneybehl/meteor-react-hammerjs',
   documentation: 'README.md'
